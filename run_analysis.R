f94aa701154a4e04f9a73aa385ab83a11ca36046
Erik Giezen <giezene@gmail.com>
2016-07-24 21:49:22 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,12 @@
+## 1. Merges the training and the test sets to create one data set.
+
+# read txt files into data frames with read.table
+features <- read.table("features.txt", header = FALSE)
+labels = read.table("activity_labels.txt", header=FALSE)
+train_subject <- read.table("subject_train.txt", header = FALSE)
+train_x <- read.table("X_train.txt", header = FALSE)
+train_y <- read.table("y_train.txt", header = FALSE)
+test_subject <- read.table("subject_test.txt", header = FALSE)
+test_x <- read.table("X_test.txt", header = FALSE)
+test_y <- read.table("y_test.txt", header = FALSE)
 
