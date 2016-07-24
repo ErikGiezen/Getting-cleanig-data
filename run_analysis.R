657702a9a091e65742f3d825e8941670cc798e10
Erik Giezen <giezene@gmail.com>
2016-07-24 22:41:07 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -10,7 +10,6 @@ test_subject <- read.table("subject_test.txt", header = FALSE)
 test_x <- read.table("X_test.txt", header = FALSE)
 test_y <- read.table("y_test.txt", header = FALSE)
 
-
 # add column names for measurments, subjects, variabels and labels
 names(train_x) <- features[,2]
 names(test_x) <- features[,2]
@@ -21,26 +20,24 @@ names(test_y) <- "activity_id"
 names(labels)[1]<-"id"
 names(labels)[2]<-"activity"
 
-
 # merge dataframes into one dataset
-train <- cbind(train_subject, train_y, train_x)
-test <- cbind(test_subject, test_y, test_x)
+train <- cbind(train_x, train_y, train_subject)
+test <- cbind(test_x, test_y, test_subject)
 merged <- rbind(train, test)
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-
 selectedcolumns <- grep("(mean|std)\\(\\)|subject|activity", names(merged))
 meanstdset <- merged[, selectedcolumns]
-
 
 ## 3. Uses descriptive activity names to name the activities in the data set.
 meanstdset <- merge(meanstdset, labels, by.x="activity_id", by.y="id")
 meanstdset <- subset(meanstdset, select = -activity_id)
 
-
 ## 4. Appropriately labels the data set with descriptive variable names.
 #already done by names(train_x) <- features[,2], names(test_x) <- features[,2]
 
 ## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-melted <- melt(meanstdset, id=c("subject","activity"))
-tidy <- dcast(melted, subject+activity ~ variable, mean)
+library(reshape2)
+melteddata <- melt(meanstdset, id=c("subject","activity"))
+eind <- dcast(melteddata, subject+activity ~ variable, mean)
+write.table(eind, file = "eind.txt", row.name=FALSE) 
